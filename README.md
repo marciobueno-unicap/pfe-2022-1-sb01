7a5c8801146b26928f144e754e379a7a99aaba48
marciobueno-unicap <marcio.bueno@unicap.br>
2022-03-16 19:36:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-smtbvu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-smtbvu)
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  const [idade, setIdade] = React.useState(0);
+  const [dtNasc, setDtNasc] = React.useState(2022);
+  const handleIdadeChange = (event) => {
+    setIdade(event.target.value);
+  }
+  const handleBtCalcularClick = () => {
+    setDtNasc(2022 - idade);
+  }
+  return (
+    <div>
+      <h1>Cálculo Ano de Nascimento</h1>
+      <p>
+        Digite sua idade: 
+        <input type="number" value={idade} onChange={handleIdadeChange} />
+       </p>
+       <p>
+        Ano de Nascimento: 
+        <input type="number" readonly value={dtNasc} />
+       </p>
+        <button onClick={handleBtCalcularClick}>
+          Calcular Data Nascimento
+        </button>
+   </div>
+  );
+}
